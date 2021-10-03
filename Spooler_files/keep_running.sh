3b27ade7276d8f654a974e090c77ae0e169bba6a
Rohit Prasad Bhatt <www.rohitprasadbhatt@gmail.com>
2021-10-04 00:24:34 +0200

    Commnads for auto activate virtual env
    Required for properly running as a  cron job on each reboot!

--- Spooler_files/keep_running.sh
+++ Spooler_files/keep_running.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-until python maintainer.py
+source /home/ubuntu/miniconda3/etc/profile.d/conda.sh
+conda activate dj_py385
+
+until python /home/ubuntu/Spooler_files/maintainer.py
 do
     echo "Restarting"
     sleep 5
