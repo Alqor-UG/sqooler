406322aa0b85198bcfc3c11ed62f27ba8cd957f4
Rohit Prasad Bhatt <www.rohitprasadbhatt@gmail.com>
2021-10-24 18:43:21 +0200

    Retry query the main server

--- Spooler_files/maintainer.py
+++ Spooler_files/maintainer.py
@@ -24,7 +24,7 @@ def new_files_exist():
 
 username = "spooler"  #'synqs_test'#
 password = "This_APP==*cool*"
-server_domain = "http://qsim-drop.herokuapp.com/"#"http://coquma-sim.herokuapp.com/"#
+server_domain = "http://coquma-sim.herokuapp.com/"#"http://qsim-drop.herokuapp.com/"#
 backends_list = ["fermions", "singlequdit", "multiqudit"]
 # Loop
 while True:
