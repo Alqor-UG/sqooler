940758478598b2dbaa2b55a6db684ed2a4af5849
fretchen <fred.jendrzejewski@gmail.com>
2023-01-26 07:43:52 +0100

    Update update_backends.py

--- update_backends.py
+++ update_backends.py
@@ -2,15 +2,22 @@ import json
 
 from utils import drpbx
 from singlequdit.spooler_singlequdit import sq_spooler
-# singlequdit
-requested_backend = "singlequdit"
+from multiqudit.spooler_multiqudit import mq_spooler
+from fermions.spooler_fermions import f_spooler
 
-# the path and name
-dbx_path = "/Backend_files/Config/" + requested_backend + "/config.json"
+backends = {
+    "singlequdit": sq_spooler,
+    "multiqudit": mq_spooler,
+    "fermions": f_spooler
+}
 
-# the content
-backend_config_dict = sq_spooler.get_configuration()
+for requested_backend, spooler in backends.items():
+    # the path and name
+    dbx_path = "/Backend_files/Config/" + requested_backend + "/config.json"
 
-result_binary = json.dumps(backend_config_dict).encode("utf-8")
-# upload the content
-drpbx.upload(result_binary, dbx_path)
+    # the content
+    backend_config_dict = spooler.get_configuration()
+
+    result_binary = json.dumps(backend_config_dict).encode("utf-8")
+    # upload the content
+    drpbx.upload(result_binary, dbx_path)
