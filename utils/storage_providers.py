9d58a4632ee6b161bc0aef3f8a080eceb16bf3f9
fretchen <fred.jendrzejewski@gmail.com>
2023-05-09 07:34:21 +0200

    Update storage_providers.py

--- utils/storage_providers.py
+++ utils/storage_providers.py
@@ -36,6 +36,7 @@ class StorageProvider(ABC):
         Get the file content from the storage
         """
 
+    @abstractmethod
     def get_job_content(self, storage_path: str, job_id: str) -> dict:
         """
         Get the content of the job from the storage. This is a wrapper around get_file_content
