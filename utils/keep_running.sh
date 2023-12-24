dec09c43f2c1b09bc0c31804b88193c0c1f66ec5
fretchen <fred.jendrzejewski@gmail.com>
2023-12-24 12:35:10 +0100

    First cleaning.

--- utils/keep_running.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source /home/ubuntu/miniconda3/etc/profile.d/conda.sh
-conda activate dj_py385
-
-until python /home/ubuntu/Spooler_files/maintainer.py
-do
-    echo "Restarting"
-    sleep 5
-done
