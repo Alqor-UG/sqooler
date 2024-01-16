c05867dee76d7fad68d9c399f01e85a61f64d324
fretchen <fred.jendrzejewski@gmail.com>
2024-01-16 21:26:59 +0100

    Cleaner tests and error messages

--- tests/test_spooler.py
+++ tests/test_spooler.py
@@ -7,11 +7,10 @@ from pydantic import ValidationError, BaseModel, Field
 
 from typing_extensions import Annotated
 import pytest
-from icecream import ic
-from sqooler.schemes import Spooler, StatusMsgDict, gate_dict_from_list
+from sqooler.schemes import Spooler, StatusMsgDict, gate_dict_from_list, ExperimentDict
 
 
-class TestExperiment(BaseModel):
+class DummyExperiment(BaseModel):
     """
     The class that defines some basic properties for a test experiment
     """
@@ -27,11 +26,39 @@ class TestExperiment(BaseModel):
     seed: Optional[int] = None
 
 
+class DummyInstruction(BaseModel):
+    """
+    The test instruction for testing the whole system.
+
+    Attributes:
+        name: The string to identify the instruction
+        wires: The wire on which the instruction should be applied
+            so the indices should be between 0 and N_MAX_WIRES-1
+        params: has to be empty
+    """
+
+    name: Literal["test"]
+    wires: Annotated[
+        list[Annotated[int, Field(ge=0, le=0)]], Field(min_length=0, max_length=1)
+    ]
+    params: Annotated[
+        list[Annotated[int, Field(ge=1, le=10)]],
+        Field(min_length=1, max_length=1),
+    ]
+
+
+def dummy_gen_circuit(experiment: dict) -> ExperimentDict:
+    """
+    A dummy function to generate a circuit from the experiment dict.
+    """
+    return ExperimentDict(header={}, shots=0, success=True, data={})
+
+
 def test_spooler_config() -> None:
     """
     Test that it is possible to get the config of the spooler.
     """
-    test_spooler = Spooler(ins_schema_dict={}, device_config=TestExperiment, n_wires=2)
+    test_spooler = Spooler(ins_schema_dict={}, device_config=DummyExperiment, n_wires=2)
 
     spooler_config = test_spooler.get_configuration()
     assert spooler_config.num_wires == 2
@@ -43,7 +70,7 @@ def test_spooler_operational() -> None:
     Test that it is possible to set the operational status of the spooler.
     """
     test_spooler = Spooler(
-        ins_schema_dict={}, device_config=TestExperiment, n_wires=2, operational=False
+        ins_schema_dict={}, device_config=DummyExperiment, n_wires=2, operational=False
     )
 
     spooler_config = test_spooler.get_configuration()
@@ -57,7 +84,7 @@ def test_spooler_add_job_fail() -> None:
     """
 
     test_spooler = Spooler(
-        ins_schema_dict={}, device_config=TestExperiment, n_wires=2, operational=False
+        ins_schema_dict={}, device_config=DummyExperiment, n_wires=2, operational=False
     )
     status_msg_draft = {
         "job_id": "Test_ID",
@@ -77,8 +104,6 @@ def test_spooler_add_job_fail() -> None:
     status_msg_dict = StatusMsgDict(**status_msg_draft)
     result_dict, status_msg_dict = test_spooler.add_job(job_payload, status_msg_dict)
     assert status_msg_dict.status == "ERROR", "Job failed"
-    ic(result_dict)
-    ic(status_msg_dict)
     assert result_dict is not None
 
 
@@ -88,7 +113,10 @@ def test_spooler_add_job() -> None:
     """
 
     test_spooler = Spooler(
-        ins_schema_dict={}, device_config=TestExperiment, n_wires=2, operational=False
+        ins_schema_dict={"test": DummyInstruction},
+        device_config=DummyExperiment,
+        n_wires=2,
+        operational=False,
     )
     status_msg_draft = {
         "job_id": "Test_ID",
@@ -99,17 +127,21 @@ def test_spooler_add_job() -> None:
 
     job_payload = {
         "experiment_0": {
-            "instructions": [],
+            "instructions": [["test", [0], [2]]],
             "num_wires": 2,
             "shots": 4,
             "wire_order": "interleaved",
         },
     }
     status_msg_dict = StatusMsgDict(**status_msg_draft)
+    # should fail gracefully as no  gen_circuit function is defined
+    result_dict, status_msg_dict = test_spooler.add_job(job_payload, status_msg_dict)
+    assert status_msg_dict.status == "ERROR", "Job failed"
+    assert status_msg_dict.error_message == "None; gen_circuit must be set"
+
+    test_spooler.gen_circuit = dummy_gen_circuit
     result_dict, status_msg_dict = test_spooler.add_job(job_payload, status_msg_dict)
     assert status_msg_dict.status == "DONE", "Job failed"
-    ic(result_dict)
-    ic(status_msg_dict)
 
 
 def test_gate_dict() -> None:
@@ -131,18 +163,80 @@ def test_gate_dict() -> None:
         gate_dict_from_list(inst_list)
 
 
+def test_spooler_check_json() -> None:
+    """
+    Test that it is possible to verify the validity of the json.
+    """
+    test_spooler = Spooler(
+        ins_schema_dict={"test": DummyInstruction},
+        device_config=DummyExperiment,
+        n_wires=2,
+        operational=False,
+    )
+
+    job_payload = {
+        "experiment_0": {
+            "instructions": [["test", [0], [2]]],
+            "num_wires": 2,
+            "shots": 4,
+            "wire_order": "interleaved",
+        },
+    }
+    # test that it works if the instructions are not valid as the key is not known
+
+    err_code, exp_ok = test_spooler.check_json_dict(job_payload)
+    assert exp_ok is True
+
+    # test that it works if the instructions are not valid
+    job_payload = {
+        "experiment_0": {
+            "instructions": [["test", [1, 2], [0.1, 0.2]]],
+            "num_wires": 2,
+            "shots": 4,
+            "wire_order": "interleaved",
+        },
+    }
+    # test that it works if the instructions are not valid as the key is not known
+
+    err_code, exp_ok = test_spooler.check_json_dict(job_payload)
+    assert exp_ok is not True
+
+
 def test_spooler_instructions() -> None:
     """
     Test that it is possible to verify the validity of the instructions.
     """
     test_spooler = Spooler(
-        ins_schema_dict={}, device_config=TestExperiment, n_wires=2, operational=False
+        ins_schema_dict={}, device_config=DummyExperiment, n_wires=2, operational=False
     )
 
     # test that it works if the instructions are not valid as the key is not known
     inst_list = [["test", [1, 2], [0.1, 0.2]]]
     err_code, exp_ok = test_spooler.check_instructions(inst_list)
     assert exp_ok is not True
-    assert err_code == "Instruction not allowed."
+    assert err_code == "No instructions allowed. Add instructions to the spooler."
+
+    # work with a valid instruction and make sure that it verifies that instructions
+    # exist
+    inst_list = [["test", [0], [1]]]
+    err_code, exp_ok = test_spooler.check_instructions(inst_list)
+    assert exp_ok is False
+    assert err_code == "No instructions allowed. Add instructions to the spooler."
+
+    # test that it works if the instructions are valid
+    test_spooler = Spooler(
+        ins_schema_dict={"test": DummyInstruction},
+        device_config=DummyExperiment,
+        n_wires=2,
+        operational=False,
+    )
+
+    inst_list = [["test", [0], [1]]]
+    err_code, exp_ok = test_spooler.check_instructions(inst_list)
+    assert exp_ok is True
+    assert err_code == ""
 
     # test that it works if the instructions are not valid
+    inst_list = [["test", [1], [1]]]
+    err_code, exp_ok = test_spooler.check_instructions(inst_list)
+    assert exp_ok is False
