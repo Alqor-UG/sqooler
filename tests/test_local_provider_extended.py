ee84d8e7d10ad8968e7eca9d4301a670487c8bde
fretchen <fred.jendrzejewski@gmail.com>
2024-05-16 06:13:38 +0200

    Testing mongo and local

--- tests/test_local_provider_extended.py
+++ tests/test_local_provider_extended.py
@@ -11,11 +11,66 @@ from decouple import config
 from pytest import LogCaptureFixture
 
 from sqooler.schemes import LocalLoginInformation
-from sqooler.storage_providers.local import LocalProviderExtended
+from sqooler.storage_providers.local import LocalCore, LocalProviderExtended
 
-from .storage_provider_test_utils import StorageProviderTestUtils
+from .storage_provider_test_utils import StorageCoreTestUtils, StorageProviderTestUtils
 
 DB_NAME = "localtest"
+
+
+class TestLocalProviderCore(StorageCoreTestUtils):
+    """
+    The class that contains all the tests for the extended local provider.
+    """
+
+    def get_login_class(self) -> Any:
+        """
+        Get the storage provider.
+        """
+        return LocalLoginInformation
+
+    def get_storage_provider(self) -> Any:
+        """
+        Get the storage provider.
+        """
+        return LocalCore
+
+    @classmethod
+    def teardown_class(cls) -> None:
+        """
+        Remove the `storage` folder.
+        """
+        shutil.rmtree("storage")
+
+    def get_login(self) -> LocalLoginInformation:
+        """
+        Pull all the login information from the environment variables.
+        """
+        return LocalLoginInformation(base_path=config("BASE_PATH"))
+
+    def test_not_active(self) -> None:
+        """
+        Test that we cannot work with the provider if it is not active.
+        """
+        self.active_tests(DB_NAME)
+
+    def test_upload_etc(self) -> None:
+        """
+        Test that it is possible to upload a file.
+        """
+        self.upload_tests(DB_NAME)
+
+    def test_file_remove(self) -> None:
+        """
+        Test that it is possible to remove a file and we raise the right errors.
+        """
+        self.remove_file_not_found_test(DB_NAME)
+
+    def test_update_raise_error(self) -> None:
+        """
+        Test that it is update a file once it was uploaded.
+        """
+        self.update_raise_error_test(DB_NAME)
 
 
 class TestLocalProviderExtended(StorageProviderTestUtils):
@@ -54,24 +109,6 @@ class TestLocalProviderExtended(StorageProviderTestUtils):
         """
 
         self.storage_object_tests(DB_NAME)
-
-    def test_not_active(self) -> None:
-        """
-        Test that we cannot work with the provider if it is not active.
-        """
-        self.active_tests(DB_NAME)
-
-    def test_upload_etc(self) -> None:
-        """
-        Test that it is possible to upload a file.
-        """
-        self.upload_tests(DB_NAME)
-
-    def test_file_remove(self) -> None:
-        """
-        Test that it is possible to remove a file and we raise the right errors.
-        """
-        self.remove_file_not_found_test(DB_NAME)
 
     def test_sign_and_verify_result(self) -> None:
         """
@@ -120,12 +157,6 @@ class TestLocalProviderExtended(StorageProviderTestUtils):
         Test that we can get the status of a backend.
         """
         self.status_tests(DB_NAME, sign=sign_it)
-
-    def test_update_raise_error(self) -> None:
-        """
-        Test that it is update a file once it was uploaded.
-        """
-        self.update_raise_error_test(DB_NAME)
 
     @pytest.mark.parametrize("sign_it", [True, False])
     def test_jobs(self, sign_it: bool) -> None:
